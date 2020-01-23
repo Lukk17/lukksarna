6362a4f2540b06789b5dceefabb1a659f41f768c
Lukk17 <luksarna@gmail.com>
2020-01-24 00:22:51 +0100

    UI refactor, code clean-up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import {WorkComponent} from "./work/work.component";
 import {CertComponent} from "./cert/cert.component";
 import {EduComponent} from "./edu/edu.component";
 import {HobbyComponent} from "./hobby/hobby.component";
+import {SkillsComponent} from "./skills/skills.component";
 
 const routes: Routes = [
   {path: '', redirectTo: "/overview", pathMatch: "full"},
   {path: 'overview', component: OverviewComponent},
   {path: 'work', component: WorkComponent},
+  {path: 'skills', component: SkillsComponent},
   {path: 'cert', component: CertComponent},
   {path: 'edu', component: EduComponent},
   {path: 'hobby', component: HobbyComponent}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { OverviewComponent } from './overview/overview.component';
-import { WorkComponent } from './work/work.component';
-import { CertComponent } from './cert/cert.component';
-import { EduComponent } from './edu/edu.component';
-import { HobbyComponent } from './hobby/hobby.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {OverviewComponent} from './overview/overview.component';
+import {WorkComponent} from './work/work.component';
+import {CertComponent} from './cert/cert.component';
+import {EduComponent} from './edu/edu.component';
+import {HobbyComponent} from './hobby/hobby.component';
+import {SkillsComponent} from './skills/skills.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { HobbyComponent } from './hobby/hobby.component';
     WorkComponent,
     CertComponent,
     EduComponent,
-    HobbyComponent
+    HobbyComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
@@ -27,4 +29,5 @@ import { HobbyComponent } from './hobby/hobby.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
